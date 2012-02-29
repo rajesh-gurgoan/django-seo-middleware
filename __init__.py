9dbe8c3fe3e60487a4c5aab384ea06f38b361117
Rajesh Kumar <rajesh.gurgoan@gmail.com>
2012-02-29 15:36:50 +0530

    keep all the files into single folder

--- __init__.py
+++
@@ -1,2 +0,0 @@
-__version__ = filter(str.isdigit, "$Revision: 2 $")
-__authors__ = ["Will Hardy <django-seo@willhardy.com.au>"]
--- urls.py
+++
@@ -1,4 +0,0 @@
-from django.conf.urls.defaults import url, patterns
-urlpatterns = patterns( 'seo.views',
-                        url( r'^get_named_url_list/$', 'get_named_url_list', name = 'get_named_url_list' ),
-                    )
